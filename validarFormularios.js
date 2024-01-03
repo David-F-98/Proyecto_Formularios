69fcf6eee1f333dfc63cfd93b06f1c524f35f155
David-F-98 <u2903149@unimilitar.edu.co>
2024-01-03 09:49:07 -0500

    Validacion de Formulario

---
+++ validarFormularios.js
@@ -0,0 +1,41 @@
+const formulario = document.getElementById('formulario-donacion');
+formulario.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    
+    const datos = {
+        correo:formulario.correo.value,
+        pais:formulario.pais.value,
+        donacion: formulario.donacion.value,
+        dia: formulario.fecha.value,
+        terminos: formulario['terminos-y-condiciones'].checked,
+    };
+
+    if(datos.correo.length  <= 2){
+        console.log('Correo invalido'); 
+        return; 
+    };
+    
+    if(datos.pais === ''){
+        console.log('Pais invalido'); 
+        return; 
+    };
+
+    if(datos.donacion === ''){
+        console.log('Selecciona una Cantidad a Donar'); 
+        return; 
+    };
+
+    if(datos.dia === ''){
+        console.log('Seleccione una fecha'); 
+        return; 
+    };
+
+    if(!datos.terminos){
+        console.log('Acepte los terminos y condiciones'); 
+        return; 
+    };
+
+    console.log(datos);
+    console.log('Enviando datos...');
+
+});
